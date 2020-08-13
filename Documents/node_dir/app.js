8cd29b39f497da331c2cd640a6f60ec87caadd06
mac <mac@macs-MBP.home>
2020-08-13 13:27:02 +0100

    Initial release

---
+++ Documents/node_dir/app.js
@@ -0,0 +1,6 @@
+var Addition= require ('./Addition.js');
+console.log(Addition.AddNumber(1, 2));
+
+var localTutor = require('./NodeTutorial.js');
+localTutor.NodeTutorial();
+//localTutor.NodeTutorial.pTutor();
